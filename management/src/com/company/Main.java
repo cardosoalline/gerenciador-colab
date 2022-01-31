5be3d5485c56af5cae7e2678c7588c06a64006c2
alline.cardoso <migowskialline@gmail.com>
2022-01-31 20:23:38 -0300

    refactor main incluindo gerente

--- management/src/com/company/Main.java
+++ management/src/com/company/Main.java
@@ -4,15 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-       Endereco endereco = new Endereco("Rua das Couves", "12", "Pomar");
-       Vendedor vendedor = new Vendedor("nome", "12121212", endereco, 1000.00);
-       vendedor.calcBonificacao(0.2);
+        Endereco endereco = new Endereco("Rua das Couves", "12", "Pomar");
+        Vendedor vendedor = new Vendedor("nome", "12121212", endereco, 1000.00);
+        vendedor.calcBonificacao(0.2);
 
-       // vendedor.setNome("teste");
+        // vendedor.setNome("teste");
 
-       System.out.println(vendedor);
+        System.out.println(vendedor);
 
         OperadorCaixa op = new OperadorCaixa("oper", "1212", endereco, 1200.00);
         System.out.println(op);
+
+        Gerente ger = new Gerente("Gerente", "34342", endereco, 56, 12.50);
+        ger.calculaRemunera();
+        ger.calcBonificacao(0.2);
+        System.out.println(ger);
     }
 }
