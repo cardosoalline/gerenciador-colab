d8af5941d47e4a1c20d13d0f46e5bd34a3209ed8
alline.cardoso <migowskialline@gmail.com>
2022-01-31 18:20:15 -0300

    refactor main

--- management/src/com/company/Main.java
+++ management/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Vendedor vendedor = new Vendedor();
+        vendedor.setNome("teste");
+
+        System.out.println(vendedor.getNome());
+
     }
 }
