930ab61f9d5b56fac519349725cae8f9d87075cc
alline.cardoso <migowskialline@gmail.com>
2022-01-31 19:47:46 -0300

    refactor main para mostrar calcBonifica

--- management/src/com/company/Main.java
+++ management/src/com/company/Main.java
@@ -4,11 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Endereco endereco = new Endereco("Rua das Couves", "12", "Pomar");
+       Endereco endereco = new Endereco("Rua das Couves", "12", "Pomar");
        Vendedor vendedor = new Vendedor("nome", "12121212", endereco, 1000.00);
+       vendedor.calcBonificacao(0.2);
+
        // vendedor.setNome("teste");
 
-      System.out.println(vendedor);
+       System.out.println(vendedor);
 
         OperadorCaixa op = new OperadorCaixa("oper", "1212", endereco, 1200.00);
         System.out.println(op);
