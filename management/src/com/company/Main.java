ae22ae967879ca0706556ffa76d8ae28c3adb03d
alline.cardoso <migowskialline@gmail.com>
2022-01-31 19:05:51 -0300

    refactor main

--- management/src/com/company/Main.java
+++ management/src/com/company/Main.java
@@ -4,10 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Vendedor vendedor = new Vendedor();
-        vendedor.setNome("teste");
+        Endereco endereco = new Endereco("Rua das Couves", "12", "Pomar");
+       Vendedor vendedor = new Vendedor("nome", "12121212", endereco, 1000.00);
+       // vendedor.setNome("teste");
 
-        System.out.println(vendedor.getNome());
+      System.out.println(vendedor);
 
+        OperadorCaixa op = new OperadorCaixa("oper", "1212", endereco, 1200.00);
+        System.out.println(op);
     }
 }
